f63bc07f571dab964002d23492818abe58bc8554
Jose Iraheta Portillo <jiraheta@gmail.com>
2025-04-11 13:07:21 -0500

    Update GlobalConstants.swift
    taking key out

--- CodCha/CodCha/GlobalConstants.swift
+++ CodCha/CodCha/GlobalConstants.swift
@@ -25,6 +25,6 @@ struct GlobalConstants {
     //ideally we would have some time of process to fetch the App-id
     //and store it in the keychain. But for the time being and the
     //time constrains we will hard code it.
-    let appid = "60bb30dc2ad52d71266dbbb8"
+    let appid = ""
     
 }
